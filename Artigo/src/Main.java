fde5a78e410ead7bfa50fe07372808e735bdc5dd
Jade Brandão <108814737+jadebrandao@users.noreply.github.com>
2024-04-05 20:55:28 -0300

    Add files via upload

---
+++ Artigo/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+
+
+        ArrayList<String> agenda = new ArrayList();
+
+        agenda.add("Anais da Academia");
+        agenda.add("Ciência e Solo");
+        agenda.add("Recursos Hídricos");
+        agenda.add("ACTA Amazonica");
+        agenda.add("ACTA Limnologica Brasileira");
+        agenda.add("Revista de Gestão de Águas");
+        agenda.add("Engenharia Agrícola e Ambiental ");
+        agenda.add("Águas Subterrâneas ");
+        agenda.add("Scielo");
+        agenda.add("PLOS One");
+
+
+    }
+}
