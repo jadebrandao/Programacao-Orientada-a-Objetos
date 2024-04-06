cfcd6e13d36b045a0dbe08feac6598ae6df52f4b
Jade Brandão <108814737+jadebrandao@users.noreply.github.com>
2024-04-05 21:06:53 -0300

    Add files via upload

---
+++ NotaAlunoCorrecao/src/correcaoat3/Main.java
@@ -0,0 +1,9 @@
+package correcaoat3;
+
+public class Main {
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno("João","123",20,20,"ADS",3);
+        aluno1.avaliaraluno();
+        aluno1.avaliarRecuperacao(80);
+    }
+}
